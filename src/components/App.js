4d9d87f7d170cbdf103fedb05a6cca078ba87f74
kranuj3122 <43272788+kranuj3122@users.noreply.github.com>
2021-02-10 19:50:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,12 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  let [flag,setFlag]=useState(false);
   return (
     <div id="main">
       // Do not alter the main div
+    	<button id="click" onClick={() => setFlag(true)}>clickToRender</button>
+			{flag && <p>Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     </div>
   );
 }
